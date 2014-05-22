2568e1d7fa206a546fe955fc34c68c04b95ed39f
Gabriel Chiconi <gabriel.chiconi@gmail.com>
2014-05-22 19:31:42 -0300

    Adicionar pesquisas por aluno, escola e trabalho

--- public_html/explore.php
+++ public_html/explore.php
@@ -1,60 +1,89 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Pesquisa </title>
-</head>
-
-<body>
-<form name="pesquisa" method="GET" action="">
-<input type="text" name="pesquisa" /> <input type='submit' value='buscar' /> 
-</form>
-
-<?php 
+<?php
 //quando o campo pesquisa está preenchido ele executa o GET
 if(isset($_GET['pesquisa']) and $_GET['pesquisa'] <> '')
 {
+	require "../conexao.class.php";
 	//a pesquisa é separada por termos(palavras pesquisadas)
 	$termos = explode(' ', $_GET['pesquisa']);
 	//contagem dos termos
 	$num = count($termos);
+
+	# checar o que se pesquisa
+	# talvez devêssemos achar um nome melhor que critério
+	if(isset($_GET["criterio"])) {
+		if($_GET["criterio"] == "curso") {
+			$oq = "curso";
+		}
+		else if($_GET["criterio"] == "escola") {
+			$oq = "escola";
+		}
+		else if($_GET["criterio"] == "aluno") {
+			$oq = "aluno";
+		}
+		else {
+			$oq = 'aluno';
+		}
+	}
+	else {
+		$oq = "aluno";
+	}
+
 	//string pesquisa na TABELA quando...
-	$pesquisar = "SELECT * FROM curso WHERE ";
+	$pesquisar = "SELECT * FROM $oq WHERE ";
 	
-	for($i=0; $i<$num; $i++)
-	{
+	for($i=0; $i < $num; $i++) {
 		// adiciona a string de pesquisa cada termos desde que ele corresponda a todos os termos pesquisados
-		$pesquisar .= "nm_curso LIKE '%".$termos[$i]."%'  ";
-		if($i<>$num-1) 
-			{ 
-				$pesquisar .= " AND "; 
-			}
+		$pesquisar .= "nm_$oq LIKE '%{$termos[$i]}%'  ";
+
+		if($i < $num - 1) {
+			$pesquisar .= " AND "; 
+		}
 	}
 	
 	//conecta ao banco
-	mysql_connect("localhost", "root", "root") or die("nao conecta");
-	mysql_select_db("keepup") or die ('sem banco');
+	$conexao = new Conexao();
 
 	//executa a busca pelas palavras
-	$pesquisando = mysql_query($pesquisar) or die("erro");
-	//conta o resultado da busca
-	$linhas = mysql_num_rows($pesquisando);
-	
-	//existindo resultado da busca são exibidos na tela 
-	if($linhas>0)
-	{
-		while($row = mysql_fetch_array($pesquisando))
-		{	
-			echo $row['nm_curso'].'<br/>';
-		}
-	}
-	else 
-	{
-		echo "Sem resultados";
-	}
+	$pesquisando = $conexao->consultar($pesquisar);
 
 }
 
 ?>
-</body>
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Pesquisa </title>
+	</head>
+	<body>
+		<?php include "header.php"; ?>
+		<section id="pesquisa">
+			<form name="pesquisa" method="GET" action="">
+				<p>
+					<input type="text" name="pesquisa">
+				</p>
+				<p>
+					<input type="radio" name="criterio" value="aluno"> Aluno
+					<input type="radio" name="criterio" value="escola"> Escola
+					<input type="radio" name="criterio" value="trabalho"> Trabalho
+					<input type="radio" name="criterio" value="curso"> Curso
+				</p>
+				<p>
+					<input type='submit' value='buscar'>
+				</p>
+			</form>
+		</section>
+		<section id="resultados">
+			<?php if(isset($pesquisando[0])) { ?>
+				<?php foreach($pesquisando as $row)	{ ?>	
+					<p>
+						<?php echo $row["nm_$oq"]; ?>
+					</p>
+				<?php } ?>
+			<?php } else { ?>
+				<p>Sem resultados</p>
+			<?php } ?>
+		</section>
+		<?php include "footer.php"; ?>
+	</body>
 </html>
