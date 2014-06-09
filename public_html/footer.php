56b538d47d1c6acda1187cf4264c5e88afc66625
Gabriel Chiconi <gabriel.chiconi@gmail.com>
2014-06-08 23:15:09 -0300

    PP

--- public_html/footer.php
+++ public_html/footer.php
@@ -1,9 +1,7 @@
     <footer>
             <div id="footer_centralizado">
                 <p> 
-                <a href="#"> Mapa do site </a> |
-                <a href="#"> Termos de uso </a> | 
-                <a href="#"> Política de privacidade </a> | 
+                <a href="pp.php"> Política de privacidade </a> | 
                 <a href="#"> Desenvolvedores  </a>
                 </p>
             		<h5> © 2014 Keep Up - Todos os direitos reservados. </h5>
